fb87c76982ee394cae70836b7a4159b14a6e9d31
Rifat Monzur <rifatmonzur@gmail.com>
2024-06-12 23:45:47 +0100

    Assign two alert to two different view.

--- MultipleAlert/ContentView.swift
+++ MultipleAlert/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     @State private var showEvenAlert:Bool = false
     @State private var showOddAlert:Bool = false
@@ -17,6 +18,9 @@ struct ContentView: View {
             TextField("Enter number: ", value: $num, format:.number)
                 .textFieldStyle(.roundedBorder)
                 .frame(maxWidth: 150)
+                .alert(isPresented: $showOddAlert, content: {
+                    Alert(title: Text("Odd"), message: Text("\(num) is odd"), dismissButton: .cancel() )
+                })
             Button("Show Alert if number is even") {
                 showEvenAlert = num % 2 == 0
                 showOddAlert = num % 2 == 1
@@ -24,9 +28,7 @@ struct ContentView: View {
             .alert(isPresented: $showEvenAlert, content: {
                 Alert(title: Text("Even"), message: Text("\(num) is even"), dismissButton: .cancel() )
             })
-            .alert(isPresented: $showOddAlert, content: {
-                Alert(title: Text("Odd"), message: Text("\(num) is odd"), dismissButton: .cancel() )
-            })
+            
             
         }
     }
