a2cc313f9621d48f3c2539670ea1a0ecf0cdcb5b
Rifat Monzur <rifatmonzur@gmail.com>
2024-06-12 23:43:43 +0100

    Add two alerts to Single View

--- MultipleAlert/ContentView.swift
+++ MultipleAlert/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showEvenAlert:Bool = false
+    @State private var showOddAlert:Bool = false
     @State private var num = 2
     
     var body: some View {
@@ -18,10 +19,15 @@ struct ContentView: View {
                 .frame(maxWidth: 150)
             Button("Show Alert if number is even") {
                 showEvenAlert = num % 2 == 0
+                showOddAlert = num % 2 == 1
             }
             .alert(isPresented: $showEvenAlert, content: {
                 Alert(title: Text("Even"), message: Text("\(num) is even"), dismissButton: .cancel() )
             })
+            .alert(isPresented: $showOddAlert, content: {
+                Alert(title: Text("Odd"), message: Text("\(num) is odd"), dismissButton: .cancel() )
+            })
+            
         }
     }
 }
