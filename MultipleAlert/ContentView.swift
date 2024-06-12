2437839303ab1f8076f3faa53f1569aed4a366e6
Rifat Monzur <rifatmonzur@gmail.com>
2024-06-12 22:52:49 +0100

    Initial Commit

---
+++ MultipleAlert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultipleAlert
+//
+//  Created by Rifat Monzur on 12/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultipleAlert/MultipleAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleAlertApp.swift
+//  MultipleAlert
+//
+//  Created by Rifat Monzur on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MultipleAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
